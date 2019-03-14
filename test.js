21b1242cf59c5aa30d98b3f203f3d5d93f3555e7
cjiava251 <zlivkosyava@gmail.com>
2019-03-14 16:48:35 +0300

    little changes with eslint

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ require("babel-register");
 
 var expect=require('expect');
 //var ec=require('./task1.js')
-import {educCourse} from "./task1.js";
-var c = educCourse.topics[0];
+import * as ec from '/p1/task1.js';
+var c = ec.topics[0];
 describe('educationCourse', function () {
     it('check answers', function () {
         expect(c).toEqual('8');
